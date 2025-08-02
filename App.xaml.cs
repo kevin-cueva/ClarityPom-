77c97f42a5cf2ee7c56775a64887c90a7b63acac
kevin-cueva <unimagneill@gmail.com>
2025-08-02 14:05:30 -0500

    Ciclo de vida de la aplicacion

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,4 +11,21 @@ public partial class App : Application
 	{
 		return new Window(new AppShell());
 	}
+	
+	//Eventos opcionales para implementar en el ciclo de vida de la aplicación. La ventana creada anteriormente 
+	//también incluye eventos de ciclo de vida que permiten realizar un seguimiento del ciclo de vida de la ventana.
+	protected override void OnStart()
+	{
+		base.OnStart();
+	}
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+    }
 }
